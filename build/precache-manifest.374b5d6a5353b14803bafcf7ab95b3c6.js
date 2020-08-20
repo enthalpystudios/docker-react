9338fb47f50f7f655c70cd6ab1793415f6e700a6
Marco Rodrigues <marco.rodrigues@enthalpystudios.com>
2020-08-20 21:13:23 +0100

    initial commit

---
+++ build/precache-manifest.374b5d6a5353b14803bafcf7ab95b3c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c5b706372f998a7abb193dabf7e9f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7394463b88830d49c9aa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7394463b88830d49c9aa",
+    "url": "/static/js/main.9201ef74.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
